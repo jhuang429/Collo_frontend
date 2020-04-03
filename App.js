46e732869280517373da97adafa7a16d7d762119
jhuang429 <jhuang9240@gmail.com>
2020-04-02 21:30:08 -0400

    icons on bottom tab

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import NewCollectionForm from './Components/NewCollectionForm';
+import NewCollectionForm from './Screens/NewCollectionForm';
 import CollectionCard from './Containers/CollectionCard';
 import NewItemForm from './Components/NewItemForm';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Collections from './Screens/Collections'
+import Settings from './Screens/Settings'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
 
@@ -23,9 +26,30 @@ export default function App() {
 
   return (
   <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={NewCollectionForm} />
-      <Tab.Screen name="Settings" component={NewCollectionForm} />
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'My Collection') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'purple',
+          inactiveTintColor: 'gray',
+        }}
+      >
+      <Tab.Screen name="My Collection" component={NewCollectionForm} />
+      <Tab.Screen name="New" component={NewCollectionForm} />
+      <Tab.Screen name="Search" component={NewCollectionForm} />
+      <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
   );
